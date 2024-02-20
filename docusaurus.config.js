58294eae9b5809dfbd70486a629cac331b097a68
codefox <isuranimalasiri@gmail.com>
2024-02-20 21:36:33 +0000

    Add docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://docs.datafella.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/df-docs/build/',
+  baseUrl: '/df-docs/build/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
