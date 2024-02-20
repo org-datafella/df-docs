9575bf33051ad96614a73f423630e9ebebf877e7
codefox <isuranimalasiri@gmail.com>
2024-02-20 21:04:44 +0000

    update datafella.org

--- df-docs/docusaurus.config.js
+++ df-docs/docusaurus.config.js
@@ -8,12 +8,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'datafella.org',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://datadocs.datafella.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/df-docs/',
