161a6b7c910a8bf0d1600b37272bf4c375a65814
codefox <isuranimalasiri@gmail.com>
2024-02-20 21:24:32 +0000

    docs.datafella.org update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://datadocs.datafella.org',
+  url: 'https://docs.datafella.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/df-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
